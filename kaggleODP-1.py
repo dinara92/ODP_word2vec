47a4708e53e4490fab633f87b5411af9af2967f9
dinara-ubuntu <dinara@kaist.ac.kr>
2016-12-20 20:15:05 +0900

    trained word2vec model with parameters from dl4j example

--- kaggleODP-1.py
+++ kaggleODP-1.py
@@ -8,7 +8,7 @@ Created on Fri Nov 25 10:33:59 2016
 
 import re
 
-with open('../allCategoriesClean.txt', 'r') as f:
+with open('../trainPages.txt', 'r') as f:
     trainSentences = [re.sub("[^\w]", " ",  line).split() for line in f]
 
 # Import the built-in logging module and configure it so that Word2Vec 
@@ -18,18 +18,19 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',\
     level=logging.INFO)
 
 # Set values for various parameters
-num_features = 200    # Word vector dimensionality                      
-min_word_count = 0   # Minimum word count                        
+num_features = 100    # Word vector dimensionality                      
+min_word_count = 5   # Minimum word count                        
 num_workers = 2       # Number of threads to run in parallel
-context = 10         # Context window size                                                                                    
+context = 5         # Context window size                                                                                    
 downsampling = 1e-3   # Downsample setting for frequent words
+seed_num = 42
 
 # Initialize and train the model (this will take some time)
 from gensim.models import word2vec
 print ("Training model...")
 model = word2vec.Word2Vec(trainSentences, workers=num_workers, \
             size=num_features, min_count = min_word_count, \
-            window = context, sample = downsampling)
+            window = context, sample = downsampling, seed = seed_num)
 
 # If you don't plan to train the model any further, calling 
 # init_sims will make the model much more memory-efficient.
@@ -37,5 +38,5 @@ model = word2vec.Word2Vec(trainSentences, workers=num_workers, \
 
 # It can be helpful to create a meaningful model name and 
 # save the model for later use. You can load it later using Word2Vec.load()
-model_name = "kaggleODP_all_categ_200features_0minwords_10context"
-model.save(model_name)
+model_name = "train_pages_dl4j_kaggleODP_pages_100features_5minwords_5context"
+#model.save(model_name)
