cb24ad8ab2edf53e38f97546df4fba35c61aceb7
dinara-ubuntu <dinara@kaist.ac.kr>
2016-12-20 20:14:22 +0900

    TO-DO: debug

--- randomForest.py
+++ randomForest.py
@@ -9,25 +9,39 @@ import pandas as pd
 import numpy as np
 
 # Read data from files 
-train = pd.read_csv( '/home/dinara/word2vec/word2vec_gensim_ODP/csv_files/trainDataVecs.csv', header=0, delimiter="\t", quoting=3 )
-test = pd.read_csv( '/home/dinara/word2vec/word2vec_gensim_ODP/csv_files/testDataVecs (copy).csv', header=0, delimiter="\t", quoting=3 )
+train = pd.read_csv( '/home/dinara/word2vec/word2vec_gensim_ODP/csv_files/trainDataVecs (smallerTrainSet).csv', header=0, delimiter="\t", quoting=3 )
+test = pd.read_csv( '/home/dinara/word2vec/word2vec_gensim_ODP/csv_files/testDataVecs (smallerTestSet).csv', header=0, delimiter="\t", quoting=3 )
 
 
 # Verify the number of reviews that were read (100,000 in total)
 print ("Read %d labeled train docs\n" % (train["id"].size))
  
-trainDataVecs = np.loadtxt('/home/dinara/word2vec/word2vec_gensim_ODP/ODP_word2vec/trainDataVecs')
-testDataVecs = np.loadtxt('/home/dinara/word2vec/word2vec_gensim_ODP/ODP_word2vec/testDataVecs')
+#trainDataVecs = np.loadtxt('/home/dinara/word2vec/word2vec_gensim_ODP/ODP_word2vec/trainDataVecs (smallerTrainSet)')
+#testDataVecs = np.loadtxt('/home/dinara/word2vec/word2vec_gensim_ODP/ODP_word2vec/testDataVecs (smallerTestSet)')
+
+#*******following are 3 ways of generating trainDataVecs and testDataVecs numpy arrays from csv files, all failed*****
+#
+
+#trainDataVecs = np.loadtxt(train, delimiter='\t', skiprows=5, usecols=["page_vector_200dim"] )
+#testDataVecs = np.loadtxt(test, delimiter='\t', skiprows=5, usecols=["page_vector_200dim"] )
+
+#trainData = pd.read_csv('/home/dinara/word2vec/word2vec_gensim_ODP/csv_files/trainDataVecs (smallerTrainSet).csv', sep="\t", usecols=['page_vector_200dim'])
+#testData = pd.read_csv('/home/dinara/word2vec/word2vec_gensim_ODP/csv_files/testDataVecs (smallerTestSet).csv', sep="\t", usecols=['page_vector_200dim'])
+#trainDataVecs = trainData.values
+#testDataVecs = testData.values
+
+#trainDataVecs = np.genfromtxt('/home/dinara/word2vec/word2vec_gensim_ODP/csv_files/trainDataVecs (smallerTrainSet).csv', delimiter='\t', usecols=["page_vector_200dim"] )
+#testDataVecs = np.genfromtxt('/home/dinara/word2vec/word2vec_gensim_ODP/csv_files/testDataVecs (smallerTestSet).csv', delimiter='\t', usecols=["page_vector_200dim"] )
 
 #fit random forest
 # Fit a random forest to the training data, using 100 trees
 
 from sklearn.ensemble import RandomForestClassifier
-forest = RandomForestClassifier(n_estimators = 2, n_jobs = 4)
+forest = RandomForestClassifier(n_estimators = 12, n_jobs = 10)
 
 print ("Fitting a random forest to labeled training data...")
-#forest = forest.fit( trainDataVecs, train["catid"] )
-forest = forest.fit( trainDataVecs, train["id"] )
+forest = forest.fit( trainDataVecs, train["catid"] )
+#forest = forest.fit( trainDataVecs, train["id"] )
 print ("Fitted, now predict...")
 
 
@@ -37,4 +51,4 @@ print ("Predicted, now output...")
 
 # Write the test results 
 output = pd.DataFrame( data={"id":test["id"], "catid":result} )
-output.to_csv( "Word2Vec_AverageVectors.csv", index=False, quoting=3 )
+#output.to_csv( "randomForest/Word2Vec_AverageVectors_smallTrainSet_12estimators_10jobs.csv", index=False, quoting=3 )
